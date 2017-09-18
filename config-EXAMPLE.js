c1386b7abe8df093624952c27c14eea6cfd69ba3
Martin Page <wookoouk@gmail.com>
2017-09-18 15:25:06 +0100

    init

---
+++ config-EXAMPLE.js
@@ -0,0 +1,7 @@
+module.exports = {
+    ena: {
+        url: 'https://www.ebi.ac.uk/ena/submit/drop-box/',
+        username: "my-ena-username",
+        password: "pianocat",
+    }
+};
---
+++ demo.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Run = require('./lib/ena/run');
+
+Run.GetRun('333')
+    .then(run => {
+        console.log('run 0', run.object);
+        run.getExperiment()
+            .then(exp => {
+                console.log('experiment', exp.object);
+                return Promise.all([
+                    exp.getStudy(),
+                    exp.getSample()
+                ])
+            })
+            .then(sampleAndStudy => {
+                console.log('study', sampleAndStudy[0].object);
+                console.log('sample', sampleAndStudy[1].object);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    })
+    .catch(err => {
+        console.error('FAIL!', err);
+    });
