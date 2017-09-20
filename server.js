06d9a5c0016ffdd1dfdb4960f16eafd6c2cb3795
Martin Page <wookoouk@gmail.com>
2017-09-20 17:12:11 +0100

    init webapp

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-const schedule = require('node-schedule');
-const checker = require('./lib/checker');
-const RunModel = require('./models/run');
-const ENA = require('./lib/ena');
+// const schedule = require('node-schedule');
+// const checker = require('./lib/checker');
+// const RunModel = require('./models/run');
+// const ENA = require('./lib/ena');
+//
+// const checkerJob = schedule.scheduleJob('0 0 * * *', function () {
+//     checker.checkRuns()
+//         .then(newRunIDS => {
+//
+//
+//
+//
+//
+//         })
+//         .catch(err => {
+//             //TODO run again in 10 mins?
+//         })
+// });
 
-const checkerJob = schedule.scheduleJob('0 0 * * *', function () {
-    checker.checkRuns()
-        .then(newRunIDS => {
+const app = require('./app');
+const log = require('./lib/log');
+const config = require('./config.js');
 
+const port = process.env.PORT || config.port || 1337;
 
-
-
-
-        })
-        .catch(err => {
-            //TODO run again in 10 mins?
-        })
+app.listen(port, '0.0.0.0', () => {
+    log.success('Listening on port', port);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
-const path = require('path');
-module.exports = {
-    entry: path.join(__dirname, 'lib', 'ena', 'index.js'),
-    // watch: true,
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    },
-    output: {
-        path: path.join(__dirname, 'lib', 'ena'),
-        filename: 'bundle.js'
-    },
-};
+// const path = require('path');
+// module.exports = {
+//     entry: {
+//         style: [path.join(__dirname, 'lib', 'ena', 'index.js')]
+//     },
+//     watch: true,
+//     module: {
+//         loaders: [
+//             {
+//                 test: /\.js$/,
+//                 loader: 'babel-loader',
+//                 query: {
+//                     presets: ['es2015']
+//                 }
+//             }
+//         ]
+//     },
+//     output: {
+//         path: path.join(__dirname, 'lib', 'ena'),
+//         filename: 'bundle.js'
+//     },
+// };
